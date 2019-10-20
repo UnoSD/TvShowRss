1cf8672534cf364ea339a9afbacbaa887a3a874a
UnoSD <unosd.dev@gmail.com>
2019-10-20 01:01:43 +0100

    Create Function App and deploy with Terraform

--- infrastructure.tf
+++ infrastructure.tf
@@ -37,8 +37,8 @@ resource "azurerm_key_vault" "tv_show_rss" {
   sku_name = "standard"
 
   access_policy {
-    tenant_id = data.azurerm_subscription.current.tenant_id
-    object_id = "cf404d16-1f86-4648-abdd-25fc63f6f7db" // Function App MI
+    tenant_id = data.azurerm_subscription.current.tenant_id # azurerm_function_app.tv_show_rss.identity.tenant_id
+    object_id = azurerm_function_app.tv_show_rss.identity.principal_id // Function App MI
 
     secret_permissions = [
       "get",
@@ -69,4 +69,94 @@ resource "azurerm_key_vault_secret" "trakt_client_secret" {
 resource "azurerm_storage_table" "series" {
   name                 = "series"
   storage_account_name = azurerm_storage_account.tv_show_rss.name
+}
+
+resource "azurerm_storage_container" "deployments_container" {
+  name                  = "deployments"
+  storage_account_name  = azurerm_storage_account.tv_show_rss.name
+  container_access_type = "private"
+}
+
+resource "azurerm_application_insights" "tv_show_rss" {
+  name                = "tvshowrssai"
+  location            = azurerm_resource_group.tv_show_rss.location
+  resource_group_name = azurerm_resource_group.tv_show_rss.name
+  application_type    = "web"
+}
+
+data "archive_file" "tv_show_rss" {
+  type = "zip"
+  # Publish linux 64 bit? dotnet publish -r linux-x64 -o bin/publish
+  # Used results of running `func azure functionapp publish test123xx`
+  source_dir = "${path.module}/bin/publish"
+  // Should be hash of itself, not timestamp
+  output_path = "${path.module}/bin/function_app.${formatdate("DDMMYYhhmmss", timestamp())}.zip"
+}
+
+resource "azurerm_storage_blob" "tv_show_rss" {
+  name = "function_app.${data.archive_file.tv_show_rss.output_md5}.zip"
+
+  storage_account_name   = azurerm_storage_account.tv_show_rss.name
+  storage_container_name = azurerm_storage_container.deployments_container.name
+
+  type   = "block"
+  source = data.archive_file.tv_show_rss.output_path
+}
+
+data "azurerm_storage_account_sas" "tv_show_rss" {
+  connection_string = azurerm_storage_account.tv_show_rss.primary_connection_string
+  https_only        = true
+
+  resource_types {
+    service   = false
+    container = false
+    object    = true
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  start  = timestamp()
+  expiry = timeadd(timestamp(), "10m")
+
+  permissions {
+    read    = true
+    write   = false
+    delete  = false
+    list    = false
+    add     = false
+    create  = false
+    update  = false
+    process = false
+  }
+}
+
+resource "azurerm_function_app" "tv_show_rss" {
+  name                      = "tvshowrssfa"
+  location                  = azurerm_resource_group.tv_show_rss.location
+  resource_group_name       = azurerm_resource_group.tv_show_rss.name
+  app_service_plan_id       = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${azurerm_resource_group.tv_show_rss.name}/providers/Microsoft.Web/serverfarms/WestEuropeLinuxDynamicPlan"
+  storage_connection_string = azurerm_storage_account.tv_show_rss.primary_connection_string
+  https_only                = true
+  enable_builtin_logging    = false
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME                 = "dotnet"
+    FUNCTIONS_EXTENSION_VERSION              = "~2"
+    FUNCTION_APP_EDIT_MODE                   = "readwrite"
+    APPINSIGHTS_INSTRUMENTATIONKEY           = azurerm_application_insights.tv_show_rss.instrumentation_key
+    AzureWebJobsStorage                      = azurerm_storage_account.tv_show_rss.primary_connection_string
+    WEBSITE_CONTENTAZUREFILECONNECTIONSTRING = azurerm_storage_account.tv_show_rss.primary_connection_string
+    WEBSITE_CONTENTSHARE                     = "tvshowrss"
+    WEBSITE_USE_ZIP                          = "https://${azurerm_storage_account.tv_show_rss.name}.blob.core.windows.net/${azurerm_storage_container.deployments_container.name}/${azurerm_storage_blob.tv_show_rss.name}${data.azurerm_storage_account_sas.tv_show_rss.sas}"
+  }
+  site_config {
+    use_32_bit_worker_process = false
+  }
+  identity {
+    type = "SystemAssigned"
+  }
 }
